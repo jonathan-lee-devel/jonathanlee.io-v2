f3afd33e7319436457bbfcccee69f46d2e8934fd
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-02-22 21:08:58 +0000

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = `Jonathan Lee's Portfolio`;
+export const SITE_DESCRIPTION = 'Welcome to my software engineering portfolio!';
